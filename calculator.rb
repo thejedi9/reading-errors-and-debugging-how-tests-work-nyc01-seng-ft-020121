428516708e6c992ae567cf32bfce4c2ac3675291
Jede Brahma <brahmajede@gmail.com>
2021-01-18 22:12:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,6 @@
-def
   first_number = (2.5)
   second_number =(70) 
   sum = "first_number" + "second_number"
   difference = "first_number" - "second_number"
   product = "first_number" * "second_number"
   quotient = "first_number" / "second_number"
-  end-of-input
